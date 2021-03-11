11130fdf3ab2bbee69e2435b36dd5be113a546a6
hackycy <qa894178522@qq.com>
2021-03-11 10:58:30 +0800

    feat：define BaseService

--- src/service/base.ts
+++ src/service/base.ts
@@ -1,1 +1,15 @@
-export class BaseService {}
+import { Plugin } from '@midwayjs/decorator';
+import { Singleton } from 'egg';
+import { Redis } from 'ioredis';
+
+/**
+ * BaseService
+ */
+export class BaseService {
+  @Plugin('redis')
+  redis: Singleton<Redis>;
+
+  getAdminRedis(): Redis {
+    return this.redis.get('admin');
+  }
+}
