5141c1e79f009595f168be80a68602f4527e8f03
hackycy <qa894178522@qq.com>
2021-03-12 17:52:32 +0800

    styles：remove InjectBullQueue in test controller

--- src/controller/home.ts
+++ src/controller/home.ts
@@ -1,20 +1,15 @@
 import { Logger } from '@midwayjs/decorator';
 import { Controller, Get, Provide } from '@midwayjs/decorator';
 import { ILogger } from '@midwayjs/logger';
-import { InjectBullQueue } from '../decorator/bull';
-import { Task } from '../queue/task';
 
 @Provide()
 @Controller('/')
 export class HomeController {
-  @InjectBullQueue(Task)
-  task;
-
   @Logger()
   logger: ILogger;
 
   @Get('/')
   async home(): Promise<string> {
-    return 'Hello Midwayjs!' + this.task.msg;
+    return 'Hello Midwayjs!';
   }
 }
