d968feafd354e83295b42097151fd9c0b2d847a8
hackycy <qa894178522@qq.com>
2021-03-16 18:24:16 +0800

    fix：remove unuse param

--- src/middleware/execption.ts
+++ src/middleware/execption.ts
@@ -13,8 +13,7 @@ export class ExecptionMiddleware implements IWebMiddleware {
       try {
         await next();
       } catch (err) {
-        const { errors } = err;
-        ctx.logger.error('[Exception]', err.message, errors);
+        ctx.logger.error('[Exception]', err.message);
         ctx.set('Content-Type', 'application/json');
         // 生产环境时 500 错误的详细错误内容不返回给客户端，因为可能包含敏感信息
         const status = err.status || 500;
