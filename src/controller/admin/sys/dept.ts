e52e3515401ba0466a4c1bdf8eb28c7ef3c6f0ff
hackycy <qa894178522@qq.com>
2021-03-18 15:10:05 +0800

    feat：migrate SysDeptController

--- src/controller/admin/sys/dept.ts
+++ src/controller/admin/sys/dept.ts
@@ -1,5 +1,160 @@
-import { Provide } from '@midwayjs/decorator';
+import {
+  ALL,
+  Body,
+  Get,
+  Inject,
+  Post,
+  Provide,
+  Query,
+} from '@midwayjs/decorator';
+import { CreateApiDoc } from '@midwayjs/swagger';
+import { res } from '../../../common/utils';
+import { AdminController } from '../../../decorator/controller';
+import { Validate } from '../../../decorator/validate';
+import {
+  CreateDeptDto,
+  DeleteDeptDto,
+  InfoDeptDto,
+  TransferDeptDto,
+  UpdateDeptDto,
+} from '../../../dto/admin/sys/dept';
+import { ResOp } from '../../../interface';
+import { AdminSysDeptService } from '../../../service/admin/sys/dept';
 import { BaseController } from '../../base';
 
 @Provide()
-export class AdminSysDeptController extends BaseController {}
+@AdminController('/sys/dept', {
+  tagName: 'AdminSysDept',
+  description: '后台系统部门模块',
+})
+export class AdminSysDeptController extends BaseController {
+  @Inject()
+  adminSysDeptService: AdminSysDeptService;
+
+  @(CreateApiDoc()
+    .summary('获取系统部门列表')
+    .respond(200, '', 'json', {
+      example: res({
+        data: [
+          {
+            createTime: '2020-08-27T03:33:19.000Z',
+            updateTime: '2020-08-27T03:33:19.000Z',
+            id: 1,
+            parentId: 2,
+            name: '思忆技术',
+            orderNum: 0,
+          },
+        ],
+      }),
+    })
+    .build())
+  @Get('/list')
+  async list(): Promise<ResOp> {
+    return res({
+      data: await this.adminSysDeptService.getDepts(this.ctx.admin.uid),
+    });
+  }
+
+  @(CreateApiDoc()
+    .summary('创建系统部门')
+    .param('创建系统部门参数')
+    .respond(200, '', 'json', { example: res() })
+    .build())
+  @Post('/add')
+  @Validate()
+  async add(@Body(ALL) createDeptDto: CreateDeptDto): Promise<ResOp> {
+    await this.adminSysDeptService.add(
+      createDeptDto.departmentName,
+      createDeptDto.parentDepartmentId
+    );
+    return res();
+  }
+
+  @(CreateApiDoc()
+    .summary('删除系统部门')
+    .param('删除系统部门参数')
+    .respond(200, '', 'json', { example: res() })
+    .build())
+  @Post('/delete')
+  @Validate()
+  async delete(@Body(ALL) deleteDeptDto: DeleteDeptDto): Promise<ResOp> {
+    // 查询是否有关联用户或者部门，如果含有则无法删除
+    const count = await this.adminSysDeptService.countUserByDeptId(
+      deleteDeptDto.departmentId
+    );
+    if (count > 0) {
+      return res({ code: 10009 });
+    }
+    const count2 = await this.adminSysDeptService.countRoleByDeptId(
+      deleteDeptDto.departmentId
+    );
+    if (count2 > 0) {
+      return res({ code: 10010 });
+    }
+    await this.adminSysDeptService.delete(deleteDeptDto.departmentId);
+    return res();
+  }
+
+  @(CreateApiDoc()
+    .summary('查询单个系统部门信息')
+    .param('系统部门参数')
+    .respond(200, '', 'json', {
+      example: res({
+        data: {
+          department: {
+            createTime: '2020-09-08T05:31:32.426Z',
+            updateTime: '2020-10-07T04:25:31.000Z',
+            id: 2,
+            parentId: 1,
+            name: 'xxx部门',
+            orderNum: 0,
+          },
+          parentDepartment: {
+            createTime: '2020-08-27T03:33:19.000Z',
+            updateTime: '2020-08-27T03:33:19.000Z',
+            id: 1,
+            parentId: null,
+            name: 'xxx部门',
+            orderNum: 0,
+          },
+        },
+      }),
+    })
+    .build())
+  @Get('/info')
+  @Validate()
+  async info(@Query(ALL) infoDeptDto: InfoDeptDto): Promise<ResOp> {
+    return res({
+      data: await this.adminSysDeptService.info(
+        parseInt(infoDeptDto.departmentId)
+      ),
+    });
+  }
+
+  @(CreateApiDoc()
+    .summary('更新系统部门')
+    .respond(200, '', 'json', { example: res() })
+    .param('更新部门信息参数')
+    .build())
+  @Post('/update')
+  @Validate()
+  async update(@Body(ALL) updateDeptDto: UpdateDeptDto): Promise<ResOp> {
+    await this.adminSysDeptService.update(updateDeptDto);
+    return res();
+  }
+
+  @(CreateApiDoc()
+    .summary('管理员部门转移')
+    .respond(200, '', 'json', { example: res() })
+    .param('转移参数')
+    .build())
+  @Post('/transfer')
+  @Validate()
+  async transfer(@Body(ALL) transferDeptDto: TransferDeptDto): Promise<ResOp> {
+    await this.adminSysDeptService.transfer(
+      transferDeptDto.userIds,
+      transferDeptDto.departmentId
+    );
+    return res();
+  }
+}
--- src/dto/admin/sys/dept.ts
+++ src/dto/admin/sys/dept.ts
@@ -7,16 +7,20 @@ import {
   IsOptional,
 } from 'class-validator';
 import { Expose } from 'class-transformer';
+import { CreateApiPropertyDoc } from '@midwayjs/swagger';
 
 export class CreateDeptDto {
+  @CreateApiPropertyDoc('系统部门名称', { example: '技术部' })
   @IsString()
   @Expose()
   departmentName: string;
 
+  @CreateApiPropertyDoc('父级部门ID', { example: 1 })
   @IsInt()
   @Expose()
   parentDepartmentId: number;
 
+  @CreateApiPropertyDoc('排序值必须为整数，最小为0', { example: 255 })
   @IsOptional()
   @IsInt()
   @Min(0)
@@ -25,18 +29,22 @@ export class CreateDeptDto {
 }
 
 export class UpdateDeptDto {
+  @CreateApiPropertyDoc('需要更新的部门id', { example: 1 })
   @IsInt()
   @Expose()
   id: number;
 
+  @CreateApiPropertyDoc('部门名称', { example: '技术部' })
   @IsString()
   @Expose()
   name: string;
 
+  @CreateApiPropertyDoc('父级部门id', { example: 2 })
   @IsInt()
   @Expose()
   parentId: number;
 
+  @CreateApiPropertyDoc('排序编号', { example: 255 })
   @IsOptional()
   @IsInt()
   @Min(0)
@@ -45,22 +53,26 @@ export class UpdateDeptDto {
 }
 
 export class DeleteDeptDto {
+  @CreateApiPropertyDoc('系统部门ID', { example: 1 })
   @IsInt()
   @Expose()
   departmentId: number;
 }
 
 export class InfoDeptDto {
+  @CreateApiPropertyDoc('系统部门ID', { example: 1 })
   @IsNumberString()
   @Expose()
   departmentId: string;
 }
 
 export class TransferDeptDto {
+  @CreateApiPropertyDoc('系统管理员ID列表', { example: [1, 2] })
   @ArrayNotEmpty()
   @Expose()
   userIds: number[];
 
+  @CreateApiPropertyDoc('转移的系统部门编号', { example: 1 })
   @IsInt()
   @Expose()
   departmentId: number;
