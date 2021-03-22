ef23002ee6959f31976decd5c09dc8fcb2a71964
hackycy <qa894178522@qq.com>
2021-03-22 10:35:56 +0800

    feat：migrate AdminSysLoginLogController

--- src/dto/comm.ts
+++ src/dto/comm.ts
@@ -1,11 +1,14 @@
 import { Expose } from 'class-transformer';
 import { Rule, RuleType } from '@midwayjs/decorator';
+import { CreateApiPropertyDoc } from '@midwayjs/swagger';
 
 export class PageSearchDto {
+  @CreateApiPropertyDoc('分页查询每页数量', { example: 10 })
   @Rule(RuleType.number().integer().min(0).default(10))
   @Expose()
   limit: number;
 
+  @CreateApiPropertyDoc('分页查询当前页数', { example: 1 })
   @Rule(RuleType.number().integer().min(1).default(1))
   @Expose()
   page: number;
