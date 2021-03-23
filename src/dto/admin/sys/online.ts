866a918a7909e6de9a444b319e25332b6ec3364f
hackycy <qa894178522@qq.com>
2021-03-23 16:48:14 +0800

    feat：add CreateApiPropertyDoc

--- src/dto/admin/sys/online.ts
+++ src/dto/admin/sys/online.ts
@@ -1,7 +1,9 @@
 import { Expose } from 'class-transformer';
 import { Rule, RuleType } from '@midwayjs/decorator';
+import { CreateApiPropertyDoc } from '@midwayjs/swagger';
 
 export class KickDto {
+  @CreateApiPropertyDoc('需要下线的角色ID')
   @Rule(RuleType.number().integer().required())
   @Expose()
   id: number;
