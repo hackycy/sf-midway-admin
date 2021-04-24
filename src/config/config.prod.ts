ec161b9f20cf6690480221c822cb51207a726e88
小小源逼 <qa894178522@qq.com>
2021-04-25 01:15:19 +0800

    fix：fix prod no config redis user

--- src/config/config.prod.ts
+++ src/config/config.prod.ts
@@ -22,6 +22,7 @@ export default (): any => {
       redis: {
         port: parseInt(process.env.REDIS_PORT) || 6379,
         host: process.env.REDIS_HOST || '127.0.0.1',
+        username: process.env.REDIS_USERNAME,
         password: process.env.REDIS_PASSWORD || '123456',
         db: 0,
       },
