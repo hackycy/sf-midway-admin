92a79aafed6c7d0b4d07bb0930d4d1209df427f4
hackycy <qa894178522@qq.com>
2021-04-28 16:39:44 +0800

    feat：add custom validate check dir name

--- src/dto/admin/file/space.ts
+++ src/dto/admin/file/space.ts
@@ -1,4 +1,27 @@
 import { Rule, RuleType } from '@midwayjs/decorator';
+import { CustomHelpers, ErrorReport } from 'joi';
+import { isEmpty } from 'lodash';
+
+// cron validate
+export const IsLegalDirExpression = (
+  value: string,
+  helpers: CustomHelpers
+): string | ErrorReport => {
+  try {
+    if (isEmpty(value)) {
+      throw new Error('dir name is empty');
+    }
+    if (value.startsWith('/')) {
+      throw new Error('dir name not allow / start');
+    }
+    if (/([\\/])\1/.test(value)) {
+      throw new Error('// this is not allow');
+    }
+    return value;
+  } catch (e) {
+    return helpers.error('dir name invalid');
+  }
+};
 
 export class GetFileListDto {
   @Rule(RuleType.string().allow('').required())
@@ -9,6 +32,10 @@ export class GetFileListDto {
 }
 
 export class MKDirDto {
-  @Rule(RuleType.string().required())
+  @Rule(
+    RuleType.string()
+      .custom(IsLegalDirExpression, '检测文件夹名称是否合法')
+      .required()
+  )
   dirName: string;
 }
