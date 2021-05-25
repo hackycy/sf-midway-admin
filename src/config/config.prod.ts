67b87f12b17f57944a299ccc761ecd76a7a52685
hackycy <qa894178522@qq.com>
2021-05-25 13:39:27 +0800

    feat：remove file space module

--- src/config/config.prod.ts
+++ src/config/config.prod.ts
@@ -1,6 +1,5 @@
 import { EggAppConfig, PowerPartial } from 'egg';
 const redisStore = require('cache-manager-ioredis');
-import * as qiniu from 'qiniu';
 
 export default (): any => {
   const config: PowerPartial<EggAppConfig> = {};
@@ -53,30 +52,6 @@ export default (): any => {
     database: process.env.MYSQL_DATABASE || 'sf-admin',
     synchronize: false,
     logging: false,
-  };
-
-  const parseZone = (zone: string) => {
-    switch (zone) {
-      case 'Zone_as0':
-        return qiniu.zone.Zone_as0;
-      case 'Zone_na0':
-        return qiniu.zone.Zone_na0;
-      case 'Zone_z0':
-        return qiniu.zone.Zone_z0;
-      case 'Zone_z1':
-        return qiniu.zone.Zone_z1;
-      case 'Zone_z2':
-        return qiniu.zone.Zone_z2;
-    }
-  };
-
-  config.qiniu = {
-    accessKey: process.env.QINIU_ACCESSKEY,
-    secretKey: process.env.QINIU_SECRETKEY,
-    domain: process.env.QINIU_DOMAIN,
-    bucket: process.env.QINIU_BUCKET,
-    zone: parseZone(process.env.QINIU_ZONE || 'Zone_z2'),
-    access: (process.env.QINIU_ACCESS_TYPE as any) || 'public',
   };
 
   // midway cache
